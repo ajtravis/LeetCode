d37febd44eae768ae472395aa08ace20796b890a
Alexander Travis <104275473+ajtravis@users.noreply.github.com>
2023-10-29 14:11:04 -0400

    Time: 107 ms (5.12%), Space: 19 MB (12.44%) - LeetHub

---
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
@@ -0,0 +1,33 @@
+class Solution:
+    
+    cv = 0
+    
+    def findTheArrayConcVal(self, nums):
+        n = len(nums)
+        if n == 0:
+            result = Solution.cv
+            Solution.cv = 0
+            return result
+        elif n == 1:
+            Solution.cv += nums[0]
+            print(nums[0], Solution.cv)
+            result = Solution.cv
+            Solution.cv = 0
+            return result
+        elif n == 2:
+            num1 = str(nums[0])
+            num2 = str(nums[-1])
+            conc = int(num1 + num2)
+            Solution.cv += conc
+            print(num1, num2, Solution.cv, conc)
+            result = Solution.cv
+            Solution.cv = 0
+            return result
+        else:
+            num1 = str(nums[0])
+            num2 = str(nums[-1])
+            conc = int(num1 + num2)
+            Solution.cv += conc
+            print(num1, num2, Solution.cv, conc)
+            new_nums = nums[1:-1]
+            return self.findTheArrayConcVal(new_nums)
