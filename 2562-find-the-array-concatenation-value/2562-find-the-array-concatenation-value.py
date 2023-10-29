db01dd2dd01fb662cc4dd78c5f56713514824a63
Alexander Travis <104275473+ajtravis@users.noreply.github.com>
2023-10-29 14:17:53 -0400

    Time: 90 ms (5.12%), Space: 18.9 MB (12.44%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
@@ -1,26 +1,44 @@
 class Solution:
     
-    cv = 0
+#     cv = 0
     
-    def findTheArrayConcVal(self, nums):
+#     def findTheArrayConcVal(self, nums):
+#         n = len(nums)
+#         if n == 1:
+#             Solution.cv += nums[0]
+#             result = Solution.cv
+#             Solution.cv = 0
+#             return result
+#         elif n == 2:
+#             num1 = str(nums[0])
+#             num2 = str(nums[-1])
+#             conc = int(num1 + num2)
+#             Solution.cv += conc
+#             result = Solution.cv
+#             Solution.cv = 0
+#             return result
+#         else:
+#             num1 = str(nums[0])
+#             num2 = str(nums[-1])
+#             conc = int(num1 + num2)
+#             Solution.cv += conc
+#             new_nums = nums[1:-1]
+#             return self.findTheArrayConcVal(new_nums)
+    def findTheArrayConcVal(self, nums, cv=0):
         n = len(nums)
         if n == 1:
-            Solution.cv += nums[0]
-            result = Solution.cv
-            Solution.cv = 0
-            return result
+            cv += nums[0]
+            return cv
         elif n == 2:
             num1 = str(nums[0])
             num2 = str(nums[-1])
             conc = int(num1 + num2)
-            Solution.cv += conc
-            result = Solution.cv
-            Solution.cv = 0
-            return result
+            cv += conc
+            return cv
         else:
             num1 = str(nums[0])
             num2 = str(nums[-1])
             conc = int(num1 + num2)
-            Solution.cv += conc
+            cv += conc
             new_nums = nums[1:-1]
-            return self.findTheArrayConcVal(new_nums)
+            return self.findTheArrayConcVal(new_nums, cv)
