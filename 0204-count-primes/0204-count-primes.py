796d02f2e0ab09be0e795a335462b76de4043905
Alexander Travis <104275473+ajtravis@users.noreply.github.com>
2023-12-01 11:43:26 -0500

    Time: 1680 ms (80.27%), Space: 55.2 MB (59.46%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,47 +1,6 @@
-import math
+
 class Solution:
-    # # primes = [2,3,5,7]
-    # # high = max(primes)
-    # def isPrime(num):
-    #         # if num < 2:
-    #         #     return False
-    #         # elif num == 2:
-    #         #     return True
-    #         # elif num % 2 == 0:
-    #         #     return False
-    #         # else:
-    #         #     i = 3 
-    #         #     while i < num/2:
-    #         #         if num % i == 0:
-    #         #             return False
-    #         #         i += 2
-    #         #         return True
-    #         flag = 0
-    #         # if num in Solution.primes:
-    #         #     return True
-    #         # elif num > Solution.high:
-    #         for i in range(2, int(math.sqrt(num))+1):
-    #             if(num%i==0):
-    #                 flag = 1
-    #                 return False
-    #         # else:
-    #         #     flag = 1
-    #         #     return False
-    #         if flag == 0:
-    #             # Solution.primes.append(int(num))
-    #             # Solution.high = max(Solution.primes)
-    #             return True
-            
     def countPrimes(self, n: int) -> int:
-         
-#         count = 0
-#         if n <= 2:
-#             return 0
-#         for x in range(2, n):
-#             if Solution.isPrime(x) == True:
-#                 count += 1   
-             
-#         return count
         if n <= 2:
             return 0
         true = [True]*(n)
