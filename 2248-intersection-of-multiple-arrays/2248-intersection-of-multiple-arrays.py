dcbc96a4c723f68fb01c2e7e1d697222b4cbb974
Alexander Travis <104275473+ajtravis@users.noreply.github.com>
2023-12-05 18:43:40 -0500

    Time: 74 ms (45.08%), Space: 16.8 MB (38.50%) - LeetHub

---
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
@@ -0,0 +1,26 @@
+class Solution:
+    def intersection(self, nums: List[List[int]]) -> List[int]:
+        lengths = [len(x) for x in nums]
+        minL = min(lengths)
+        index = lengths.index(minL)
+        small = nums[index]
+        result = []
+        i = 0
+        while i < len(small):
+            n = small[i]
+            j = 0
+            flag = 0
+            while j < len(nums):
+                # if j == index:
+                #     j+=1
+                if n not in nums[j]:
+                    flag = 1
+                    break
+                j+=1
+            if flag == 0:
+                result.append(n)
+            i+=1
+        result.sort()
+        return result
+            
+        
