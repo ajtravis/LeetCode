6fa532070b1b35f3f59144229045aabb8d267b55
Alexander Travis <104275473+ajtravis@users.noreply.github.com>
2025-04-01 09:45:17 -0400

    Time: 38 ms (76.71%) | Memory: 53 MB (69.86%) - LeetSync

---
+++ 2901-factorial-generator/factorial-generator.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @yields {number}
+ */
+function* factorial(n) {
+    const memo = new Map(); //memo Map
+    
+    
+    function factorialRecursive(n) {
+        if (memo.has(n)) {
+            return memo.get(n);
+        }
+
+        let result;
+        if (n <= 1) {
+            result = 1;
+        } else {
+            result = n * factorialRecursive(n - 1);
+        }
+
+        memo.set(n, result);
+        return result;
+    }
+    
+    if (n === 0) {
+        yield 1;
+    } else {
+        for (let i = 1; i <= n; i++) {
+            yield factorialRecursive(i);
+        }
+    }
+    }
